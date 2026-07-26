dda2cdce9fa09c231369cb642676efa9dbfcfc58
André Silva <andresilva.webdev@gmail.com>
2026-07-26 18:00:10 +0000

    3o commit

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+if (!supabaseUrl || !supabaseKey) {
+  console.error('⚠️ Atenção: As variáveis de ambiente do Supabase não estão definidas.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
